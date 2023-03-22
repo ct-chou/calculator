7f1d12bc491a27034562c896ed990712e33a406d
ct-chou <63345621+ct-chou@users.noreply.github.com>
2023-03-22 19:29:43 +0800

    Feat: Enable clear, delete and numbers
    reflects on screen

--- calculator.js
+++ calculator.js
@@ -12,3 +12,39 @@ const operate = function(operation, x, y) {
         return x/y;
     }
 }
+
+const keyPad = document.querySelectorAll('button');
+const displayScreen = document.getElementById('displayScreen');
+
+let displayValue = 0;
+console.log(displayValue);
+
+keyPad.forEach(keyEntry => {
+    keyEntry.addEventListener('click', (e) => {
+        // console.log(keyEntry.id);
+        if(keyEntry.id == 'clear') {
+            displayValue = 0;
+        }
+        else if(keyEntry.id == 'delete') {
+            if(displayValue != 0) {
+                displayValue = displayValue.slice(0,-1);    
+            }
+            if(displayValue == '') {
+                displayValue = 0;
+            }
+        }
+        else if(keyEntry.className == 'operator') {
+            console.log(keyEntry.id);
+        }
+
+        else {
+            if(displayValue == 0) {
+                displayValue = keyEntry.id.toString();
+            }
+            else {
+                displayValue += keyEntry.id.toString();
+            }
+        }
+        displayScreen.textContent = displayValue;
+    });
+});
